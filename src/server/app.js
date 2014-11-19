346f73fbc203851f6e64ecbae317d5d042d3667c
Luis Benavides <luis.i.benavides.-ND@disney.com>
2014-11-19 17:26:22 -0500

    Generating sitemap.xml when the server starts

--- src/server/app.js
+++ src/server/app.js
@@ -4,19 +4,24 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 8626;
+var prerender = require('prerender-node').set('prerenderToken', 'xLmdV8Et5kavIbrtBaJE');
+var crawler = require('./crawler');
+var sitemap = require('sitemap');
+var Promise = require("bluebird");
+var fs = require('fs');
+var url = require('url');
 
 var environment = process.env.NODE_ENV;
-
 console.log('PORT=' + port);
 console.log('NODE_ENV=' + environment);
 
-app.use(require('prerender-node').set('prerenderToken', 'xLmdV8Et5kavIbrtBaJE'));
+app.use(prerender);
 app.use('/', express.static('./src/client'));
 app.use('/', express.static('./'));
 
-//As a fallback, any route that would otherwise throw a 404 (Not Found) will return the
-//home page, so that the client, in HTML5 mode, attempts to navigate to a potential deep link.
-app.get('*', function(request, response){
+// Any route that would otherwise throw a 404 (Not Found) will return the
+// home page, so that the client, in HTML5 mode, attempts to navigate to a potential deep link.
+app.get('*', function (request, response) {
     response.sendfile('./src/client/index.html');
 });
 
@@ -25,4 +30,21 @@ app.listen(port, function () {
     console.log('env = ' + app.get('env') +
     '\n__dirname = ' + __dirname +
     '\nprocess.cwd = ' + process.cwd());
+
+    Promise.promisify(crawler.crawl)('http://localhost:' + port).then(function (urls) {
+        var homeUrl = process.env.HOME_URL || 'http://localhost:' + port;
+        var sm = sitemap.createSitemap({
+            hostname: homeUrl
+        });
+
+        urls.forEach(function(aUrl){
+            console.log('URL: ' + aUrl);
+            sm.add({url: url.parse(aUrl).path.slice(1)});
+        });
+
+        sm.toXML(function (xml) {
+            // Write to a file
+            fs.writeFile('sitemap.xml', xml);
+        });
+    });
 });
---
+++ src/server/crawler.js
@@ -0,0 +1,46 @@
+var Browser = require('zombie');
+var url = require('url');
+
+var browserOpts = {
+    waitFor: 500,
+    loadCSS: false,
+    runScripts: true
+}
+
+function crawlPage(idx, urls, callback) {
+    if (idx < urls.length) {
+        var uri = urls[idx];
+        var browser = new Browser(browserOpts);
+        browser.on("error", function (error) {
+            callback(error, null);
+        });
+        browser.visit(uri).
+            then(function () {
+                browser.wait(function () {
+                    var links = browser.queryAll('a');
+                    links.forEach(function (link) {
+                        var href = link.getAttribute('href');
+                        if (href && href !== '#' && href !== '/') {
+                            var absUrl = url.resolve(uri, href);
+                            if (urls.indexOf(absUrl) < 0) {
+                                urls.push(absUrl);
+                            }
+                        }
+                    });
+                    crawlPage(idx + 1, urls, callback);
+                });
+            }).
+            catch(function (error) {
+                callback(error, null);
+            });
+    } else {
+        callback(null, urls);
+    }
+}
+
+module.exports = {
+    crawl: function (startingUrl, callback) {
+        startingUrl = startingUrl || 'http://localhost';
+        crawlPage(0, [startingUrl], callback);
+    }
+};
